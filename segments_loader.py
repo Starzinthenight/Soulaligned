7f4da1b763612c2226c8bb20bb9557962b73eace
Simone Wallace <starzinthenight81@gmail.com>
2025-05-01 01:35:12 +0100

    Updated SEGMENTS_URL to latest gist version

--- segments_loader.py
+++ segments_loader.py
@@ -1,10 +1,11 @@
 import json
 import requests
 
-SEGMENTS_URL = 'https://gist.githubusercontent.com/Starzinthenight/d4178399c7f1c43972fcfc3fb53c6818/raw/4816e12595185d7db5116b89683e1e200155d281/segments.json'
+SEGMENTS_URL = 'https://gist.githubusercontent.com/Starzinthenight/d4178399c7f1c43972fcfc3fb53c6818/raw/ae0acb830ffa2f8f8e4806b72047e29a62e2802a/segments.json'
 
 def load_segments():
     """Download and return segments JSON from your Gist."""
     response = requests.get(SEGMENTS_URL)
     response.raise_for_status()
     return json.loads(response.text)
+
